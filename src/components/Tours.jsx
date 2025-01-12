11cdbf7773800325b8c2b85b51294d95fd32f175
Aditi-gig <adutm15@gmail.com>
2025-01-12 14:33:18 +0530

    Update Tours.jsx the name

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -5,7 +5,7 @@ const Tours = ({tours,removeTour}) => {
   return (
    <div className='container'>
      <div>
-     <h2 className='title'>Plan With ove</h2>
+     <h2 className='title'>Plan With Love</h2>
     </div>
     <div className='cards'>{
         tours.map((tour)=>{
